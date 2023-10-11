c2b2cb7d80ef183593a944f56b3aa8a65eb67bfa
Alexis Dorado Muñoz <alexis11dimen@gmail.com>
2023-10-11 16:30:02 -0500

    Añadida lógica de registro de fechas en el método SaveChangesAsync para auditoria

--- src/Infrastructure/Persistence/TaskDbContext.cs
+++ src/Infrastructure/Persistence/TaskDbContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tareas.Domain;
+using Tareas.Domain.Common;
 
 namespace Tareas.Infrastructure.Persistence
 {
@@ -12,7 +13,33 @@ namespace Tareas.Infrastructure.Persistence
     {
         public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options)
         {
-            // Constructor que recibe opciones de DbContext.
+        }
+
+        // Este método anula la funcionalidad predeterminada de Entity Framework Core para guardar cambios en la base de datos.
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // Recorre todas las entidades rastreadas por el contexto de Entity Framework.
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch (entry.State)
+                {
+                    // Si la entidad es nueva (Added), establece la fecha de creación en la fecha y hora actual.
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+
+                    // Si la entidad se está modificando (Modified), actualiza la fecha de última modificación a la fecha y hora actual.
+                    case EntityState.Modified:
+                        entry.Entity.LastUpdateDate = DateTime.Now;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            // Llama al método SaveChangesAsync de la clase base para guardar los cambios en la base de datos.
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
