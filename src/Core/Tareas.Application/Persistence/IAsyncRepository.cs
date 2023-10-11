4ce315b8c352b51b309158f3b26c4241e59f4d71
Alexis Dorado Muñoz <alexis11dimen@gmail.com>
2023-10-11 14:01:03 -0500

    Se crea interfaz repositorio generica y se crea clase para represetnar un patron de trabajo

---
+++ src/Core/Tareas.Application/Persistence/IAsyncRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas.Application.Persistence
+{
+    // Interfaz genérica para repositorios asíncronos.
+    public interface IAsyncRepository<T> where T : class
+    {
+        // Recupera todos los elementos de tipo T de forma asíncrona.
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        // Recupera elementos de tipo T que cumplen con un predicado y opcionalmente realiza un ordenamiento y carga relacionada.
+        Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>>? predicate,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
+            string? includeString,
+            bool disableTracking = true
+        );
+
+        // Recupera elementos de tipo T que cumplen con un predicado y opcionalmente realiza un ordenamiento, carga relacionada y tracking.
+        Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>>? predicate,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            List<Expression<Func<T, object>>>? includes = null,
+            bool disableTracking = true
+        );
+
+        // Recupera un elemento de tipo T que cumple con un predicado y opcionalmente realiza una carga relacionada y tracking.
+        Task<T> GetEntityAsync(
+            Expression<Func<T, bool>>? predicate,
+            List<Expression<Func<T, object>>>? includes = null,
+            bool disableTracking = true
+        );
+
+        // Recupera un elemento de tipo T por su ID de forma asíncrona.
+        Task<T> GetByIdAsync(int id);
+
+        // Agrega un elemento de tipo T de forma asíncrona.
+        Task<T> AddAsync(T entity);
+
+        // Actualiza un elemento de tipo T de forma asíncrona.
+        Task<T> UpdateAsync(T entity);
+
+        // Elimina un elemento de tipo T de forma asíncrona.
+        Task DeleteAsync(T entity);
+
+    }
+}
---
+++ src/Core/Tareas.Application/Persistence/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Tareas.Application.Persistence
+{
+    // La interfaz IUnitOfWork se utiliza para representar un patrón de unidad de trabajo.
+    // Este patrón se utiliza para agrupar múltiples operaciones de base de datos en una única transacción.
+    public interface IUnitOfWork : IDisposable
+    {
+        // El método Repository<TEntity> se utiliza para obtener un repositorio para una entidad específica.
+        // Este repositorio se utiliza para realizar operaciones de lectura y escritura en la entidad.
+        IAsyncRepository<TEntity> Repository<TEntity>() where TEntity : class;
+
+        // El método Complete() se utiliza para confirmar y finalizar la transacción de la unidad de trabajo.
+        // Devuelve el número de cambios guardados en la base de datos como resultado de la transacción.
+        Task<int> Complete();
+    }
+}
