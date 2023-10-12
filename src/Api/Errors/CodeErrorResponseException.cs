779e76e30ffd3a325be8a3c0ae9702cf7301dff0
Alexis Dorado Muñoz <alexis11dimen@gmail.com>
2023-10-12 00:48:03 -0500

    Se crea y configura la clase CodeErrorResponseException para agregar detalles tecnicos al mensaje de error

---
+++ src/Api/Errors/CodeErrorResponseException.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Tareas.Api.Errors
+{
+    // Se extiende la clase CodeErrorResponse y se utiliza para representar respuestas de error personalizadas en formato JSON que incluyen detalles adicionales.
+    public class CodeErrorResponseException : CodeErrorResponse
+    {
+        [JsonProperty(PropertyName = "details")]
+        public string? Details { get; set; }  // Representa detalles técnicos adicionales relacionados con el error.
+
+        public CodeErrorResponseException(int statusCode, string[]? message = null, string? details = null)
+            : base(statusCode, message)
+        {
+            Details = details;  // Asigna los detalles adicionales proporcionados al objeto de respuesta de error.
+        }
+    }
+}
