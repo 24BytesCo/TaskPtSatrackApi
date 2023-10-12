ecadf4378d8e3aba0dc3de6c894771440ed3a589
Alexis Dorado Muñoz <alexis11dimen@gmail.com>
2023-10-12 00:27:23 -0500

    Añadida la clase NotFoundException para manejar excepciones de entidad no encontrada y la clase BadRequestException para  manejar excepciones relacionadas con solicitudes incorrectas.

---
+++ src/Core/Tareas.Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tareas.Application.Exceptions
+{
+    // Esta clase se utiliza para manejar excepciones relacionadas con solicitudes incorrectas.
+    public class BadRequestException : ApplicationException
+    {
+        // Constructor de la clase que toma un parámetro 'mensaje'.
+        // 'mensaje' representa un mensaje descriptivo que se muestra al lanzar la excepción.
+        public BadRequestException(string mensaje) : base(mensaje)
+        {
+        }
+    }
+}
+
---
+++ src/Core/Tareas.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tareas.Application.Exceptions
+{
+    // Esta clase NotFoundException se utiliza para manejar excepciones cuando una entidad no se encuentra en la base de datos.
+    public class NotFoundException : ApplicationException
+    {
+        // Constructor de la clase que toma dos parámetros: 'nombre' y 'key'.
+        // 'nombre' se refiere al nombre de la entidad que no se encontró, y 'key' es la clave o identificador de la entidad no encontrada.
+        public NotFoundException(string nombre, object key) : base($"La entidad {nombre} {key} no ha sido encontrado")
+        {
+        }
+    }
+}
