46b7ed5c5cf29db48d5afc2b9756c512d536785f
Alexis Dorado Muñoz <alexis11dimen@gmail.com>
2023-10-12 00:18:47 -0500

    Añadida la clase ValidationException para manejar errores de validación

---
+++ src/Core/Tareas.Application/Behaviors/UnhandledExceptionBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas.Application.Behaviors
+{
+    // La clase UnhandledExceptionBehavior es un comportamiento para gestionar excepciones no manejadas
+    // que pueden ocurrir durante el procesamiento de solicitudes de la aplicación.
+    public class UnhandledExceptionBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Procesa la solicitud y pasa al siguiente comportamiento.
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                // Registra la excepción no manejada en el sistema de registro.
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Se produjo una excepción para la solicitud {Name} {@Request}", requestName, request);
+
+                // Lanza una nueva excepción para indicar que la solicitud de la aplicación tiene errores.
+                throw new Exception("Application Request con errores");
+            }
+        }
+    }
+}
---
+++ src/Core/Tareas.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results; // Importamos el espacio de nombres necesario.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tareas.Application.Exceptions
+{
+    // Esta clase ValidationException se utiliza para representar excepciones relacionadas con errores de validación.
+    public class ValidationException : ApplicationException
+    {
+        // La propiedad Errors almacena los errores de validación como un diccionario donde la clave es el nombre de la propiedad y el valor es un array de mensajes de error.
+        public IDictionary<string, string[]> Errors { get; }
+
+        // Constructor predeterminado que establece un mensaje predeterminado.
+        public ValidationException() : base("Se presentaron uno o más errores de validación")
+        {
+            // Inicializa la propiedad Errors como un nuevo diccionario vacío.
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        // Constructor sobrecargado que toma una lista de fallos de validación (ValidationFailure) como argumento.
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            // Transforma la lista de fallos en un diccionario donde las claves son los nombres de las propiedades y los valores son arrays de mensajes de error.
+            Errors = failures
+                .GroupBy(failure => failure.PropertyName, failure => failure.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+    }
+}
